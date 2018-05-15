7f10c6691b746cc1173c5f29727e05760fe4977c
syc <doingself@163.com>
2018-05-15 10:38:50 +0800

    mybatis generator setting

--- ssmProject/ssmArtifact-web/src/main/java/com/syc/generator/MybatisGenerator.java
+++ ssmProject/ssmArtifact-web/src/main/java/com/syc/generator/MybatisGenerator.java
@@ -3,14 +3,20 @@ package com.syc.generator;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MybatisGenerator {
-    public void generator() throws Exception{
+
+    public static void main(String[] args) {
+        System.out.println("mybatis generator class main start");
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         //MybatisGenerator.class.getResource("") 当前MybatisGenerator的目录
@@ -21,20 +27,28 @@ public class MybatisGenerator {
         File configFile = new File(MybatisGenerator.class.getResource("/generatorConfig.xml").getFile());
 //        File configFile = new File("src/main/resources/generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(configFile);
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        myBatisGenerator.generate(null);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("mybatis generator class main start");
+        Configuration config = null;
         try {
-
-            MybatisGenerator generator = new MybatisGenerator();
-            generator.generator();
-
-        } catch (Exception e) {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("mybatis generator class main end");
